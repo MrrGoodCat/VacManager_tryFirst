8fe96a73b27a524ae72c49cbfce3281bdc7c1a86
MrrGoodCat <mrrgoodcat@gmail.com>
2016-11-23 02:19:11 +0200

    Add button and email verification

--- www/js/js.js
+++ www/js/js.js
@@ -1,29 +1,23 @@
-
-
-function valid(form)
-    {
+function valid (form)
+{
         var fail = false;
-        var name = form.name.value;
+        var email = form.email.value;
+        var adr_pattern = /[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}/i;
         var password = form.password.value;
-        var RePassword = form.RePassword.value;
-        var state = form.state.value;
-        if(name == "" || name == " ")
-            {
-                fail = "You haven't write your name";
-            }
-        else if(password == "")
-            {
-                fail = "You haven't write your password.";
-            }
-        else if(RePassword != password)
-            {
-                fail = "The passwords are not matched.";
-            }
-        if(fail)
-            {
-                alert(fail);
-            }
-        else {
-            window.location = "index.html";
+        if(adr_pattern.test(email) == false)
+        {
+            fail = "You haven't write your name";
         }
-    }
+        else if(password == "")
+        {
+            fail = "You haven't write your password.";
+        }
+        if(fail)
+        {
+            alert(fail);
+        }
+        else
+        {
+            window.location.href="index.html";
+        }
+}
